dc51edf6922f03a59c742957c37a5a08e02fc813
Jubjub422 <ross.stovenour@gmail.com>
2022-01-25 13:40:28 -0600

    added function/html for tags

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,8 +1,13 @@
 import React from "react"
 import { Route } from "react-router-dom"
+import { Tags } from "./tags/Tags"
 
 export const ApplicationViews = () => {
   return (
-    <h1 >Welcome to Rare Publishing</h1>
+    <>
+    <Route path="/tags">
+      <Tags/>
+    </Route>
+    </>
   )
 }
