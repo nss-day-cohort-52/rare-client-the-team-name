5b9eb83b9e2bba694329dcb2c1f22ba4d92c2b17
jmason87 <joewebdev87@gmail.com>
2022-01-25 13:47:32 -0600

    adding route, fetches, and displays for categories

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,8 +1,14 @@
 import React from "react"
 import { Route } from "react-router-dom"
+import { Categories } from "./categories/categroyList"
 
 export const ApplicationViews = () => {
   return (
-    <h1 >Welcome to Rare Publishing</h1>
+    <>
+      {/* <h1 >Welcome to Rare Publishing</h1> */}
+      <Route path="/categories">
+        <Categories />
+      </Route>
+    </>
   )
 }
