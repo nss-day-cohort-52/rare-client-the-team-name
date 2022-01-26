c519b5d0c5f228deb3868689f2af8b1153e1702f
Jubjub422 <ross.stovenour@gmail.com>
2022-01-26 10:30:27 -0600

    fixed typo with user first name

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -5,7 +5,7 @@ export default ({ user }) => (
     <section className="user">
         <h3 className="user_detail_link">
             <Link to={`/users/${user.id}`}>
-                { user.firstName } {user.last_name}
+                { user.first_name } {user.last_name}
             </Link>
         </h3>
         <div> {user.username} </div>
