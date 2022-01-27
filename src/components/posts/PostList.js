db12c9f04e471bdb6838bbbbc227baaa74563c45
jmason87 <joewebdev87@gmail.com>
2022-01-27 13:04:14 -0600

    commit for merge

--- src/components/posts/PostList.js
+++ src/components/posts/PostList.js
@@ -3,17 +3,31 @@ import { getPosts } from "./PostManager"
 import Post from "./Post"
 import { PostFilters } from "./PostFilters"
 
-export const PostList = () => {
+export const PostList = ({ postss }) => {
     const [ posts, setPosts ] = useState([])
+    const [ searchedPost, setSearch] = useState("")
 
     useEffect(()=> {
         getPosts().then(p => setPosts(p))
     }, [])
+
+    useEffect(() => {
+        if (searchedPost !== "") {
+        searchByTitle(searchedPost).then(data => setPosts(data))
+        } else {
+            setPosts(postss)
+        }
+    }, [searchedPost, postss])
     
     return (
         <>
             <PostFilters setPosts={setPosts}/>
-            <input type="text" placeholder="Search by Title..." name="search" />
+            <input type="text" placeholder="Search by Title..." name="search" onKeyUp={
+                (event) => {
+                    const searchTerm = event.target.value
+                    setSearch(searchTerm)
+                }
+            }/>
             <div className="posts">
                 {
                     posts.map(post => <Post key={post.id} post={post} />)
