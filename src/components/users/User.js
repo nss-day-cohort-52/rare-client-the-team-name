7993b6272145f721e90e711ca61b65d0b57eaf0a
jmason87 <joewebdev87@gmail.com>
2022-01-28 10:39:34 -0600

    commit for merge

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -2,13 +2,14 @@ import React from "react"
 import { Link } from "react-router-dom"
 
 export default ({ user }) => (
-    <section className="user">
-        <h3 className="user_detail_link">
-            <Link to={`/users/${user.id}`}>
-                { user.first_name } {user.last_name}
-            </Link>
-        </h3>
-        <div> {user.username} </div>
-        <div> {user.email} </div>
-    </section>
+    <div className="card">
+                <div className="card-content  m-6">
+                        <Link className="title is-4" to={`/users/${user.id}`}>
+                            {user.first_name} {user.last_name}
+                        </Link>
+                    <div> {user.username} </div>
+                    <div> {user.email} </div>
+                </div>
+    </div>
 )
+
--- src/components/users/UserDetails.js
+++ src/components/users/UserDetails.js
@@ -45,15 +45,15 @@ export const UserDetails = () => {
         if (user.id === currentUserId) {
             return ""
         } else if (subscriptionId === 0) {
-            return <button type="submit" onClick={() => newSubscription()} className="btn btn-primary">
+            return <button type="submit" onClick={() => newSubscription()} className="button">
                 Subscribe
             </button>
         } else if (subscriptionId !== 0) {
             return <button type="submit"
                 onClick={() => {
                     deleteSubscription(subscriptionId)
-                    .then(() => getSubsByFollower(currentUserId))
-                    .then(setUserSubscriptions)
+                        .then(() => getSubsByFollower(currentUserId))
+                        .then(setUserSubscriptions)
                 }}
                 className="btn btn-primary">
                 Unsubscribe
@@ -63,17 +63,23 @@ export const UserDetails = () => {
 
     return (
         <>
-            <div className="user-image">
-                <img src={user.profile_image_url} alt="user profile image" />
+            <div className="card is-one-third">
+                <div className="card-image">
+                    {/* <figure class="image is-4by3"> */}
+                        <img src={user.profile_image_url} alt="user profile image" />
+                    {/* </figure> */}
+                </div>
+                <div className="media-content">
+                    <p className="title is-4">{user.first_name} {user.last_name}</p>
+                </div>
+                <div className="content">
+                    <div> Email: {user.email}</div>
+                    <div> Bio: {user.bio} </div>
+                    <div>Created on: {user.created_on}</div>
+                    <div> Username: {user.username} </div>
+                    {subscribeButton()}
+                </div>
             </div>
-            <section className="user-detail">
-                <h3 className="user-name">{user.first_name} {user.last_name}</h3>
-                <div> Email: {user.email}</div>
-                <div> Bio: {user.bio} </div>
-                <div>Created on: {user.created_on}</div>
-                <div> Username: {user.username} </div>
-            </section>
-            {subscribeButton()}
         </>
     )
 }
