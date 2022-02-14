0901aea9b6da16df90fe4980047d5aefbfdacc73
LeahDaniel <leah.daniel1997@gmail.com>
2022-02-14 15:33:13 -0600

    user views functioning

--- src/components/users/UserDetails.js
+++ src/components/users/UserDetails.js
@@ -5,18 +5,17 @@ import { addSubscription, deleteSubscription, getSubsByFollower } from "../subsc
 import { getCurrentUser, getSingleUser } from "./UserManager"
 
 export const UserDetails = () => {
-    const [rareUser, setUser] = useState({})
+    const [rareUser, setRareUser] = useState({})
     const [userSubscriptions, setUserSubscriptions] = useState([])
     const [subscriptionId, setSubscriptionId] = useState(0)
     const [currentUser, setCurrentUser] = useState({})
     const { userId } = useParams()
-    const parsedId = parseInt(userId)
     const history = useHistory()
 
     useEffect(() => {
-        getSingleUser(parsedId).then(setUser)
+        getSingleUser(userId).then(setRareUser)
         getCurrentUser().then(setCurrentUser)
-    }, [parsedId])
+    }, [userId])
 
     // useEffect(() => {
     //     getSubsByFollower(currentUserId).then(setUserSubscriptions)
@@ -32,35 +31,35 @@ export const UserDetails = () => {
     // }, [userSubscriptions])
 
 
-    const newSubscription = () => {
-        const date = new Date()
-        addSubscription({
-            follower_id: currentUserId,
-            author_id: parsedId,
-            created_on: date.toDateString()
-        })
-            .then(() => history.push("/"))
-    }
+    // const newSubscription = () => {
+    //     const date = new Date()
+    //     addSubscription({
+    //         follower_id: currentUserId,
+    //         author_id: parsedId,
+    //         created_on: date.toDateString()
+    //     })
+    //         .then(() => history.push("/"))
+    // }
 
-    const subscribeButton = () => {
-        if (user.id === currentUser.id) {
-            return ""
-        } else if (subscriptionId === 0) {
-            return <button type="submit" onClick={() => newSubscription()} className="button mr-3 mt-3">
-                Subscribe
-            </button>
-        } else if (subscriptionId !== 0) {
-            return <button type="submit"
-                onClick={() => {
-                    deleteSubscription(subscriptionId)
-                        .then(() => getSubsByFollower(currentUser.id))
-                        .then(setUserSubscriptions)
-                }}
-                className="btn btn-primary">
-                Unsubscribe
-            </button>
-        }
-    }
+    // const subscribeButton = () => {
+    //     if (rareUser.id === currentUser.id) {
+    //         return ""
+    //     } else if (subscriptionId === 0) {
+    //         return <button type="submit" onClick={() => newSubscription()} className="button mr-3 mt-3">
+    //             Subscribe
+    //         </button>
+    //     } else if (subscriptionId !== 0) {
+    //         return <button type="submit"
+    //             onClick={() => {
+    //                 deleteSubscription(subscriptionId)
+    //                     .then(() => getSubsByFollower(currentUser.id))
+    //                     .then(setUserSubscriptions)
+    //             }}
+    //             className="btn btn-primary">
+    //             Unsubscribe
+    //         </button>
+    //     }
+    // }
 
     return (
         <>
@@ -71,14 +70,14 @@ export const UserDetails = () => {
                         <div>
 
                             <img src={rareUser.profile_image_url} alt="user profile image" className="image is-128x128 mr-3" />
-                            {subscribeButton()}
+                            {/* {subscribeButton()} */}
                         </div>
                         <div className="content">
-                            <p className="title is-4">{rareUser.user.first_name} {rareUser.user.last_name}</p>
-                            <div> Email: {rareUser.user.email}</div>
+                            <p className="title is-4">{rareUser.user?.first_name} {rareUser.user?.last_name}</p>
+                            <div> Email: {rareUser.user?.email}</div>
                             <div> Bio: {rareUser.bio} </div>
-                            <div>Created on: {rareUser.user.date_joined}</div>
-                            <div> Username: {rareUser.user.username} </div>
+                            <div>Created on: {rareUser.user?.date_joined}</div>
+                            <div> Username: {rareUser.user?.username} </div>
                         </div>
                     </div>
                 </div>
